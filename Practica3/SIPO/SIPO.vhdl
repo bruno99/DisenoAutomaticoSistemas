9a3d3c72d197c7ed4c97c98306bac32d07f145d5
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-03-29 15:45:18 +0100

    Update SIPO.vhdl

--- Practica3/SIPO/SIPO.vhdl
+++ Practica3/SIPO/SIPO.vhdl
@@ -1,4 +1,5 @@
 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -15,26 +16,34 @@ architecture Behavioral of SIPO is
           Port (CLK, RST, D: in std_logic; 
                 Q: out std_logic);
       end component; 
+      
+      signal D : std_logic_vector(N_FF-1 downto 0);
+
 begin
 
-GEN: for n in 0 to N_FF-1 generate    
- if(n = '0') then
+GEN: for n in 0 to N_FF-1 generate  
+ 
+ FIRST: if (n = 0) generate
+
     MY_FFS1 : flipflop port map(
     CLK => CLK,
     RST => RST,
     D => D_in,
     Q => D_out(n) 
     );   
-    else
+  end generate FIRST;
+  
+  ELSES: if (n > 0) generate
     MY_FFS : flipflop port map(
     CLK => CLK,
     RST => RST,
-    D => D_in,
+    D => D(n-1),
     Q => D_out(n) 
     );  
-end if;
- 
- 
-end generate;
+ end generate ELSES;   
+
+end generate GEN;
+
+D_out <= std_logic_vector(D(N_FF-1 downto 0)); 
 
 end Behavioral;
