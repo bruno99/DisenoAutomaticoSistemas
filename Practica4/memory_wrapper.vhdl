1de3147a114e268e699cf9d80e2b28415d2f772c
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-05-04 10:42:14 +0100

    Update memory_wrapper.vhdl

--- Practica4/memory_wrapper.vhdl
+++ Practica4/memory_wrapper.vhdl
@@ -1,11 +1,10 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity memory_wrapper is
   PORT (
-    clk : IN STD_LOGIC;
+    clk, ena: IN STD_LOGIC;
     addr : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
     data : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
   );
@@ -16,6 +15,7 @@ architecture Behavioral of memory_wrapper is
 component blk_mem is
  PORT (
     clka : IN STD_LOGIC;
+    ena : IN STD_LOGIC;
     addra : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
     douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
   );
@@ -25,6 +25,7 @@ begin
 --instanciarlo
 MEMORY_GEN: blk_mem port map(
 clka => clk,
+ena => ena,
 addra => addr,
 douta => data
 );
